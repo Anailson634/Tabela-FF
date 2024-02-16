3f893789e5e4c76ca984354b5971abbf302122d1
Anailson634 <anonymoushack838@gmail.com>
2024-02-16 16:27:05 -0300

    back-end finalizado

--- painel-back.js
+++ painel-back.js
@@ -5,14 +5,17 @@ var list_jgs=[]
 
 
 lista_us.addEventListener('click', function(event) {
-    var kill=document.querySelector("#kills")
-    var ponto=document.querySelector("#pontos")
+    var kill0=document.querySelector("#kills0").value
+    var kill1=document.querySelector("#kills1").value
+    var ponto0=document.querySelector("#pontos0").value
+    var ponto1=document.querySelector("#pontos1").value
     
     if (event.target.tagName === 'LI') {
         let index = Array.from(lista_us.children).indexOf(event.target)
-        list_jgs[index]['Kill']=kill.value
-        list_jgs[index]['Pontos']=ponto.value
+        list_jgs[index]['Kill']=parseInt(kill0)+parseInt(kill1)
+        list_jgs[index]['Pontos']=parseInt(ponto0)+parseInt(ponto1)
     }
+    kill0=kill1=ponto0=ponto1 = ''
 })
 
 function pontuar(){
@@ -22,13 +25,20 @@ function pontuar(){
         list_jgs.push({
             "Nome": v,
             "Kill": 0,
-            "Pontos": 0
+            "Pontos": 0,
         })
         item.innerText=v
         lista_us.appendChild(item)
     }
 }
+function calcular_win() {
+    for (const v of list_jgs) {
+        v.Pontos=parseInt(v.Kill)+parseInt(v.Pontos)
+    }
+    list_jgs.sort((a, b) => b.Pontos - a.Pontos);
+}
 function enviar_dados() {
+    calcular_win()
     fetch("http://127.0.0.1:5000", {
         method: "PUT",
         headers: {
